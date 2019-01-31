4b7e9ca3a98e9620df571cb2b5977fa980723bd7
Loudon <loudonii@163.com>
2019-01-31 11:10:24 +0800

    Change default lang
    Set the default language as Simplified Chinese

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
     dialog: DialogService,
     translate: TranslateService) {
     dialog.setRootViewContainerRef(vRef);
-    translate.setDefaultLang('en');
-    translate.use(localStorage.getItem('lang') || 'en');
+    translate.setDefaultLang('cn');
+    translate.use(localStorage.getItem('lang') || 'cn');
   }
 }
